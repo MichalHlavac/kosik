5a46099f7abb69e970e27a0418af203629b4c191
Michal Hlaváč <48187991+MichalHlavac@users.noreply.github.com>
2019-08-14 05:49:09 +0200

    Update shoppingCart.js

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -3,14 +3,15 @@ import {SCContext} from './SCContext';
 
 export default ()=>{
     const[items, setItem]=useContext(SCContext);
-    const [totalP,setTotalP]=useState(0)
+    const [total,setTotal]=useContext(TContext);
     const shopingCart = items.map(item =>{
         const cloneItems= [...items];
+        const oldTotal = total;
         const ID = items.indexOf(item);
         const newItems =(newValue)=>{cloneItems.splice(ID,1,{"name":item.name,"price":item.price,"quantity":newValue})
                 return(cloneItems)};
-        const updateItem = e=>setItem(newItems(e.target.value));
-        const deleteItem = ()=>setItem(items.filter(i=>i.name!==item.name));
+        const updateItem = e=>setItem(newItems(e.target.value)&setTotal(oldTotal-rowPrice+(item.price*e.target.value)));
+        const deleteItem = ()=>setItem(items.filter(i=>i.name!==item.name)& setTotal(oldTotal-rowPrice));
         const rowPrice= item.price*item.quantity;
         return(<div key={item.name}>
                 <h2>{item.name}</h2>
@@ -22,6 +23,6 @@ export default ()=>{
                 </div>)});
     return(<div>
             {shopingCart}
-            <p>celkem{" "}{totalP}Kč</p>
+            <p>celkem{" "}{total}Kč</p>
         </div>);
 }
