4b8861c0e839c21d229d15cbe29e746f3ec83c7b
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-09-06 14:44:52 +0200

    +gitignore -remove indexOf

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -5,13 +5,12 @@ import {SCContext,TContext} from './SCContext';
 export default ()=>{
     const[items, setItem]=useContext(SCContext);
     const [total,setTotal]=useContext(TContext);
-    const shopingCart = items.map(item =>{
+    const shopingCart = items.map((item,I) =>{
         const cloneItems= [...items];
         const oldTotal = Number(total);
-        const ID = items.indexOf(item);
         const rowPrice=item.price*item.quantity;
         const Price =item.price;
-        const newItems =(newValue)=>{cloneItems.splice(ID,1,{"name":item.name,"price":item.price,"quantity":newValue})
+        const newItems =(newValue)=>{cloneItems.splice(I,1,{"name":item.name,"price":item.price,"quantity":newValue})
                 return(cloneItems)};
         const updateItem = e=>setItem(newItems(e.target.value))& setTotal(oldTotal-rowPrice+(e.target.value*Price));
         const deleteItem = ()=>setItem(items.filter(i=>i.name!==item.name))& setTotal(oldTotal-rowPrice);
